37a945b00c44ed9092234c2ce2b155df8684bf7d
sidhi-sid <sidhisidh469@gmail.com>
2020-05-28 19:08:33 +0530

    setup done

--- hardware/IV/IV.ino
+++ hardware/IV/IV.ino
@@ -18,7 +18,19 @@ boolean isEmpty;
 
 void setup() {
   // put your setup code here, to run once:
+ WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  pinMode(D1,OUTPUT);  
+  pinMode(ldr,INPUT);
 }
 
 void loop() {
