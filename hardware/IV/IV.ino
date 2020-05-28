0150c509eeba7c9c53c17d3c3265ffcf567fce88
sidhi-sid <sidhisidh469@gmail.com>
2020-05-28 18:49:31 +0530

    Connected with firebase and Wifi

--- hardware/IV/IV.ino
+++ hardware/IV/IV.ino
@@ -4,6 +4,14 @@
 #include <WiFiUdp.h>
 #include <FirebaseArduino.h>
 
+//Credentials for connection to firebase
+#define FIREBASE_HOST "smart-iv-hackon.firebaseio.com"
+#define FIREBASE_AUTH ""
+
+//Credentials for connection to Wi-Fi
+#define WIFI_SSID "Galaxy"
+#define WIFI_PASSWORD "ssingh9970"
+
 
 
 void setup() {
